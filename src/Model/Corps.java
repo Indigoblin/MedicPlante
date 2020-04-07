9f804d17d61bd1f3677657463a158431330c96ca
Maxime Cansell <maxime.cansell@gmail.com>
2020-04-07 16:18:16 +0200

    creation des classes Corps et Plante

---
+++ src/Model/Corps.java
@@ -0,0 +1,12 @@
+package Model;
+
+public class Corps {
+
+    private int idPlante;
+    private String nom;
+
+    public Corps(int idPlante, String nom) {
+        this.idPlante = idPlante;
+        this.nom = nom;
+    }
+}
--- src/Model/Plante.java
+++ src/Model/Plante.java
@@ -1,4 +1,21 @@
 package Model;
 
 public class Plante {
+    private int idPlante;
+    private String nom;
+    private String bienfait;
+    private String photo;
+    private String posologie;
+    private String description;
+    private String avertissement;
+
+    public Plante(int idPlante, String nom, String bienfait, String photo, String posologie, String description, String avertissement) {
+        this.idPlante = idPlante;
+        this.nom = nom;
+        this.bienfait = bienfait;
+        this.photo = photo;
+        this.posologie = posologie;
+        this.description = description;
+        this.avertissement = avertissement;
+    }
 }
