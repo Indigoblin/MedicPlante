609e8c2d11970134f64fedb3179be7e713688fd3
CloudyoErzod <cloudyo.pro@gmail.com>
2020-04-14 11:40:14 +0200

    MAJ BDD

--- src/Sample/BDD_MedicPlante.sql
+++ src/Sample/BDD_MedicPlante.sql
@@ -1,6 +1,20 @@
 DROP DATABASE if exists bdd_medicplant;
 CREATE DATABASE bdd_medicplant;
 
+
+
+CREATE TABLE bdd_medicplant.Corps(
+                      idCorps   Int Auto_increment NOT NULL PRIMARY KEY,
+                      NomPartie Varchar (250) NOT NULL
+);
+
+CREATE TABLE bdd_medicplant.Symptome(
+                         idSymptome  Int Auto_increment NOT NULL PRIMARY KEY,
+                         nomSymptome Varchar (250) NOT NULL,
+                         id_Corps   Int NOT NULL
+    ,CONSTRAINT Corps_FK FOREIGN KEY (id_Corps) REFERENCES bdd_medicplant.Corps(idCorps)
+);
+
 CREATE TABLE bdd_medicplant.Plante
 (
                        idPlante      Int Auto_increment NOT NULL PRIMARY KEY,
@@ -12,18 +26,4 @@ CREATE TABLE bdd_medicplant.Plante
                        Avertissement Text NOT NULL ,
                        id_Symptome   Int NOT NULL
     ,CONSTRAINT Symptome_FK FOREIGN KEY (id_Symptome) REFERENCES bdd_medicplant.Symptome(idSymptome)
-);
-
-
-CREATE TABLE bdd_medicplant.Symptome(
-                         idSymptome  Int Auto_increment NOT NULL PRIMARY KEY,
-                         nomSymptome Varchar (250) NOT NULL,
-                         id_Corps   Int NOT NULL
-    ,CONSTRAINT Corps_FK FOREIGN KEY (id_Corps) REFERENCES bdd_medicplant.Corps(idCorps)
-);
-
-
-CREATE TABLE bdd_medicplant.Corps(
-                      idCorps   Int Auto_increment NOT NULL PRIMARY KEY,
-                      NomPartie Varchar (250) NOT NULL
 );
