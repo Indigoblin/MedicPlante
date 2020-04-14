c4ce9299fca9ac338ca3441810b0fcc6f62e46a7
mathieuPailot <mathieu.pailot@uha.fr>
2020-04-14 11:27:28 +0200

    Maj BDD

--- src/Sample/BDD_MedicPlante.sql
+++ src/Sample/BDD_MedicPlante.sql
@@ -9,49 +9,21 @@ CREATE TABLE bdd_medicplant.Plante
                        Bienfait      Text NOT NULL ,
                        Photo         Varchar (250) NOT NULL ,
                        Posologie     Text NOT NULL ,
-                       Avertissement Text NOT NULL
+                       Avertissement Text NOT NULL ,
+                       id_Symptome   Int NOT NULL
+    ,CONSTRAINT Symptome_FK FOREIGN KEY (id_Symptome) REFERENCES bdd_medicplant.Symptome(idSymptome)
 );
 
 
 CREATE TABLE bdd_medicplant.Symptome(
                          idSymptome  Int Auto_increment NOT NULL PRIMARY KEY,
-                         nomSymptome Varchar (250) NOT NULL
+                         nomSymptome Varchar (250) NOT NULL,
+                         id_Corps   Int NOT NULL
+    ,CONSTRAINT Corps_FK FOREIGN KEY (id_Corps) REFERENCES bdd_medicplant.Corps(idCorps)
 );
 
 
 CREATE TABLE bdd_medicplant.Corps(
                       idCorps   Int Auto_increment NOT NULL PRIMARY KEY,
                       NomPartie Varchar (250) NOT NULL
-);
-
-
-CREATE TABLE bdd_medicplant.MotClef(
-                        idMotClef  Int Auto_increment NOT NULL PRIMARY KEY,
-                        nomMotClef Varchar (250) NOT NULL
-);
-
-
-CREATE TABLE bdd_medicplant.Guerie(
-                       idGuerie   Int NOT NULL PRIMARY KEY,
-                       idSymptome Int NOT NULL,
-                       idPlante   Int NOT NULL
-    ,CONSTRAINT Guerie_Symptome_FK FOREIGN KEY (idSymptome) REFERENCES bdd_medicplant.Symptome(idSymptome)
-    ,CONSTRAINT Guerie_Plante0_FK FOREIGN KEY (idPlante) REFERENCES bdd_medicplant.Plante(idPlante)
-);
-
-
-CREATE TABLE bdd_medicplant.Possede(
-                        idPossede Int NOT NULL PRIMARY KEY,
-                        idSymptome Int NOT NULL,
-                        idCorps    Int NOT NULL
-    ,CONSTRAINT Possede_Symptome_FK FOREIGN KEY (idSymptome) REFERENCES bdd_medicplant.Symptome(idSymptome)
-    ,CONSTRAINT Possede_Corps0_FK FOREIGN KEY (idCorps) REFERENCES bdd_medicplant.Corps(idCorps)
-);
-
-CREATE TABLE bdd_medicplant.Avoir(
-                      idAvoir  Int NOT NULL PRIMARY KEY,
-                      idMotClef  Int NOT NULL,
-                      idSymptome Int NOT NULL
-    ,CONSTRAINT Avoir_MotClef_FK FOREIGN KEY (idMotClef) REFERENCES bdd_medicplant.MotClef(idMotClef)
-    ,CONSTRAINT Avoir_Symptome0_FK FOREIGN KEY (idSymptome) REFERENCES bdd_medicplant.Symptome(idSymptome)
 );
